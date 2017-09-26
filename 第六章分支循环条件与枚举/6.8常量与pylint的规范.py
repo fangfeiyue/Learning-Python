a5deca5a624d73a6a6b684ce4edf8d3da8be0e67
FangFeiyue <18766131540@163.com>
2017-09-26 17:01:05 +0800

    6.8常量与pylint的规范

--- 第六章分支循环条件与枚举/6.8常量与pylint的规范.py
+++ 第六章分支循环条件与枚举/6.8常量与pylint的规范.py
@@ -1,1 +1,18 @@
-a = raw_input()
+'''
+    login
+'''
+ACCOUNT = 'ffy'
+PASSWORD = '123456'
+
+print'please input your account'
+USER_NAME = raw_input()
+print'please input your password'
+USER_PASSWORD = raw_input()
+
+if USER_NAME and USER_PASSWORD:
+    if USER_NAME == ACCOUNT and USER_PASSWORD == PASSWORD:
+        print'load success'     
+    else:
+        print'account or password is error'
+else:
+    print'account or passworld can not empty'
