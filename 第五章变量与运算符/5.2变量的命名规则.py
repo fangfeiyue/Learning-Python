4c5a6963d7560de3c3362c5c2d2d724c09eb55fc
FangFeiyue <18766131540@163.com>
2017-09-26 10:00:22 +0800

    值类型与引用类型

---
+++ 第五章变量与运算符/5.2变量的命名规则.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+a = [1, 2, 3, 4, 5, 6];
+b = a;
+a[0] = '2';
+print(a);
+print(b);
---
+++ 第五章变量与运算符/5.3值类型与引用类型.py
@@ -0,0 +1,39 @@
+#coding=utf-8
+#值类型
+"""a = 1;
+b = 1;
+a = 2;
+print(a);#2
+print(b);#1
+"""
+
+"""str1 = '123';
+str2 = str1;
+str1 = "3434";
+print(str1);
+print(str2);"""
+
+"""tuple1 = (1, 2, 3, 4);
+tuple2 = tuple1;
+tuple1 = (4, 5, 6, 23);
+print(tuple1);
+print(tuple2);"""
+
+#引用类型
+"""list1 = [1, 2, 3, 4];
+list2 = list1;
+list1[0] = 'sd';
+print(list1);
+print(list2);"""
+
+set1 = {1, 2, 3, 4, 5};
+set2 = set1;
+set1[1] = "23";
+print(set1);
+print(set2);
+
+"""dic1 = {1:2};
+dic2 = dic1;
+dic1[0]="哈哈";
+print(dic1);
+print(dic2);"""
