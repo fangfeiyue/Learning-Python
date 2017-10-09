9152af8cacb0f05a636d66a0b7ede240ea8f86e4
FangFeiyue <18766131540@163.com>
2017-10-09 18:21:24 +0800

    update readme

--- 第六章分支循环条件与枚举/6.11思考题解答与改变定势思维.py
+++ 第六章分支循环条件与枚举/6.11思考题解答与改变定势思维.py
@@ -3,4 +3,14 @@
 '''
 
 a = input();
-print(type(a));
+
+a = int(a);
+
+if a == 1:
+    print('apple')
+elif a == 2:
+    print('orange')
+elif a == 3:
+    print('banana')
+else:
+    print('other')
